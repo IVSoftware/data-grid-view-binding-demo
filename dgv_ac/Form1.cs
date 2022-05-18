8dde95e9ab43198f43d3d2ca5c702323ddab03d6
Tom Gregor <tgregor@ivsoftware.com>
2022-05-18 14:27:20 -0600

    pgp

--- dgv_ac/Form1.cs
+++ dgv_ac/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -32,6 +33,7 @@ namespace dgv_ac
             {
                 DataSource.Add(new Record { Number = i, FileName = $"MyFile_{i}.txt" });
             }
+            dataGridView1.Refresh();
 
             // Once the first record is added, the Columns information
             // is available and we can do column formatting.
@@ -43,11 +45,53 @@ namespace dgv_ac
 
             var checkboxColumn = dataGridView1.Columns[nameof(Record.IsChecked)];
             checkboxColumn.HeaderText = string.Empty;
-            checkboxColumn.Width = 40;
+            checkboxColumn.Width = 60;
+            checkboxColumn.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            attachPanel();
 
             dataGridView1.CellClick += onCellClick;
             dataGridView1.CellContentClick += onCellContentClick;
+            dataGridView1.CellPainting += onCellPainting;
         }
+
+        private void attachPanel()
+        {
+            var checkBox = new CheckBox()
+            {
+                Width = 20,
+                Height = 20,
+            };
+            dataGridView1.Controls.Add(headerPanel);
+            headerPanel.ColumnCount = 3;
+            headerPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10));
+            headerPanel.ColumnStyles.Add(new ColumnStyle(SizeType.AutoSize));
+            headerPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10));
+            headerPanel.Controls.Add(checkBox, 1, 0);
+            headerPanel.AutoSize = true;
+            checkBox.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+        }
+
+        // https://docs.microsoft.com/en-us/dotnet/desktop/winforms/controls/how-to-host-controls-in-windows-forms-datagridview-cells?view=netframeworkdesktop-4.8
+
+
+        private void onCellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            if ((e.RowIndex == -1) && (e.ColumnIndex != -1))
+            {
+                switch (dataGridView1.Columns[e.ColumnIndex].Name)
+                {
+                    case nameof(Record.IsChecked):
+                        var headerCellLocation = dataGridView1.GetCellDisplayRectangle(e.ColumnIndex, -1, true);
+                        headerPanel.Location = headerCellLocation.Location;
+                        headerPanel.BackColor = Color.Red;
+                        headerPanel.Size = headerCellLocation.Size;
+                        break;
+                }
+            }
+        }
+
+        // https://www.aspsnippets.com/Articles/Add-Check-all-CheckBox-in-Header-row-of-DataGridView-using-C-and-VBNet-in-Windows-Application.aspx
+        TableLayoutPanel headerPanel = new TableLayoutPanel();
 
         /// <summary>
         /// Detect check box click and end the edit mode in this case.
